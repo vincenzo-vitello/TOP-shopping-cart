181492720a200c5e1e6f5515a129ab12be8187e5
Vincenzo Vitello <vincenzo.vitello@icloud.com>
2025-04-22 16:29:56 +0200

    initial home design

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import style from "./home.module.css";
 
 function Home() {
-  const [homeData, setHomeData] = useState([]);
+  const [homeData, setHomeData] = useState({});
 
   useEffect(() => {
     fetch("/data/home.json")
@@ -10,7 +12,23 @@ function Home() {
       .catch((err) => console.log(err));
   }, []);
 
-  return <>Home</>;
+  if (!homeData.hero) return <div>Loading...</div>;
+  console.log(homeData);
+  return (
+    <>
+      <header className={style.hero}>
+        <div className={style.heroContent}>
+          <h1>{homeData.hero.headline}</h1>
+          <p>{homeData.hero.subheadline}</p>
+          <Link className={style.cta} to={"../Shop"}>
+            {homeData.hero.ctaText}
+          </Link>
+        </div>
+        <div className={style.heroBackground}>
+          <img src={homeData.hero.backgroundImage} alt="" />
+        </div>
+      </header>
+    </>
+  );
 }
-
 export default Home;
