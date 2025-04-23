1234479f5f3e0be0ad5e218308f5b7c380767a8a
Vincenzo Vitello <vincenzo.vitello@icloud.com>
2025-04-23 16:39:24 +0200

    added responsivity to home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -28,6 +28,37 @@ function Home() {
           <img src={homeData.hero.backgroundImage} alt="" />
         </div>
       </header>
+      <section className={style.featuredProduct}>
+        <h2>{homeData.featuredProduct.title}</h2>
+        <div className={style.featuredProductContainer}>
+          <div className={style.imageWrapper}>
+            <span className={style.label}>
+              {homeData.featuredProduct.label}
+            </span>
+            <img src={homeData.featuredProduct.image} alt="" />
+          </div>
+          <div className={style.productInfo}>
+            <p>{homeData.featuredProduct.description}</p>
+            <Link className={style.cta} to={homeData.featuredProduct.link}>
+              Shop Now
+            </Link>
+          </div>
+        </div>
+      </section>
+      <section className={style.benefits}>
+        <h2>{homeData.benefits.headline}</h2>
+        <div className={style.benefitsContainer}>
+          {homeData.benefits.map((benefit, index) => {
+            return (
+              <div key={index} className={style.benefit}>
+                <img src={benefit.icon} alt="" />
+                <h3>{benefit.title}</h3>
+                <p>{benefit.description}</p>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </>
   );
 }
