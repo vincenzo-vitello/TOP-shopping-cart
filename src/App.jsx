489dbf6db180cb77a3adbb3f7765135ed5088053
Vincenzo Vitello <vincenzo.vitello@icloud.com>
2025-04-24 14:22:51 +0200

    fixed scroll to top when navigatin between pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import Shop from "./pages/Shop";
 import ProductPage from "./pages/Product";
 import Cart from "./pages/Cart";
 import Layout from "./components/Layout";
-
+import ScrollToTop from "./components/ScrollToTop";
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
